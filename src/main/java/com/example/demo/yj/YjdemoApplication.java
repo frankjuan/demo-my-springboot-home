3af5c75efeb8329319ca9e23a26a20234be22f16
frank <yujun_lucky@126.com>
2018-07-26 00:24:28 +0800

    firstcommit

---
+++ src/main/java/com/example/demo/yj/YjdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.yj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjdemoApplication.class, args);
+	}
+}
